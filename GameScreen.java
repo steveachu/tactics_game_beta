61265fcd02f916d232b3f773d2d7bad77d407bb9
steveachu <steveachu@users.noreply.github.com>
2016-10-24 00:30:20 -0700

    Re-worked game engine
    Major updates include collision detection for water, new sounds added, and encapsulation of code into functions.

--- GameScreen.java
+++ GameScreen.java
@@ -22,7 +22,8 @@ public class GameScreen implements Screen {
 	private OrthographicCamera camera;
         private SpriteBatch batch;
         private Rectangle mPlayer;
-        private char[][] mWorld;
+        private Tile[][] mWorld;
+        private char[][] mPlayerPosition;
         
         private final int SCREEN_WIDTH_PX = 800;
         private final int SCREEN_HEIGHT_PX = 480;
@@ -31,15 +32,19 @@ public class GameScreen implements Screen {
         private final int FRAME_ROWS = 1;
         private final int WATER_COLS = 2;
         private final int WATER_ROWS = 1;
-        
-        private Sound mSelectionSound;
+        private final int MTN_COLS = 3;
+        private final int MTN_ROWS = 3;
         
         private Tile mWaterTile;
         private Tile mGrassTile;
         private Tile mDirtTile, mDirtTile2, mDirtTile3;
         private Tile mBridgeTile;
         private Tile mOrangeTreeTile;
-        private Tile mMountainTile;
+               
+        private Sound mInvalidSound;
+        private Sound mSelectionSound;
+        
+        private Texture mMountainSheet;
         
         private Animation mPlayerAnimation;
         private Texture mPlayerSheet;
@@ -51,14 +56,19 @@ public class GameScreen implements Screen {
         private TextureRegion mCurrentWaterFrame;
         private TextureRegion[] mWaterFrames;
         
+        private Texture mMtnTexture;
+        
         private Texture mMenuBar;
         
         private float mStateTime;
 
+        private int mPlayerX;
+        private int mPlayerY;
+        
     public GameScreen(final MemoryWars gam) {
         this.game = gam;
         // Generate the map
-        mWorld  = new char[][] {
+        /*mWorld  = new short[][] {
             {3,3,3,3,3,3,1,1,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5},
             {3,3,3,3,3,3,1,1,1,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5},
             {3,3,4,3,3,3,3,1,1,1,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5},
@@ -69,58 +79,116 @@ public class GameScreen implements Screen {
             {3,3,3,3,4,3,6,3,3,6,6,1,1,1,0,0,0,0,0,0,0,0,0,0,0},
             {6,3,6,4,3,6,6,3,3,3,3,3,1,1,0,0,0,0,0,0,0,0,0,0,0},
             {6,3,6,6,3,3,4,3,3,3,3,6,1,1,1,1,0,0,0,0,0,0,0,0,0},
-            {3,4,3,3,3,3,3,3,4,3,3,3,3,1,1,1,1,1,0,0,0,0,0,0,0},
-            {3,3,3,3,3,3,6,6,4,3,3,3,3,3,6,1,1,1,1,1,1,1,0,0,0},
-            {3,3,3,4,3,3,6,6,4,4,3,3,3,4,3,1,1,1,1,1,1,1,0,0,0},
+            {3,4,3,7,6,6,3,3,4,3,3,3,3,1,1,1,1,1,0,0,0,0,0,0,0},
+            {3,3,3,6,6,6,6,6,4,3,3,3,3,3,6,1,1,1,1,1,1,1,0,0,0},
+            {3,3,3,6,6,6,6,6,4,4,3,3,3,4,3,1,1,1,1,1,1,1,0,0,0},
             {3,3,3,6,3,4,3,3,3,3,6,3,3,3,4,3,6,1,1,1,1,1,1,0,0},
             {3,3,6,6,6,6,3,3,3,3,3,3,6,6,6,6,6,1,1,1,1,1,1,0,0}
-        };
+        };*/               
         
         mMenuBar = new Texture(Gdx.files.internal("menu_bar.png"));                        
+        
         initSpriteAnimation();
         initTextures();
+        initSounds(); 
         
-        mSelectionSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
-        mFirstLevelMusic = Gdx.audio.newMusic(Gdx.files.internal(
-         "first_level.mp3"));
-        mFirstLevelMusic.setLooping(true);  
+        mWorld = new Tile[25][15];
+        for (int i = 0; i < mWorld.length; i++) {
+            for (int j = 0; j < mWorld[i].length; j++) {
+                mWorld[i][j] = mDirtTile;
+            }
+        }
+        
+        mWorld[5][5] = mWaterTile;
+        mWorld[5][6] = mWaterTile;
+        mWorld[5][7] = mWaterTile;
+        mWorld[5][8] = mWaterTile;
+        mWorld[5][9] = mWaterTile;
+        
+        /*
+        mWorld = new Tile[][] {
+            {mWaterTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile},
+            {mDirtTile,mDirtTile,mWaterTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile},
+            {mDirtTile,mDirtTile,mWaterTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile},
+            {mDirtTile,mDirtTile,mWaterTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile},
+            {mDirtTile,mDirtTile,mDirtTile,mDirtTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile},
+            {mDirtTile,mDirtTile,mDirtTile,mDirtTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile},
+            {mDirtTile,mDirtTile,mDirtTile,mDirtTile,mWaterTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile},
+            {mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile},
+            {mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile},
+            {mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mWaterTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile},
+            {mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile},
+            {mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile},
+            {mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mWaterTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile},
+            {mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile},
+            {mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mWaterTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile,mDirtTile}            
+        }; 
+        */
+        /*
+        mWorld = new Tile[][] {
+            {mBridgeTile, mDirtTile, mDirtTile},
+            {mDirtTile, mWaterTile, mDirtTile},
+            {mDirtTile, mWaterTile, mGrassTile} // TOP RIGHT
+        };*/
+                        
         // Create the camera and spritebatch
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, SCREEN_WIDTH_PX, SCREEN_HEIGHT_PX);
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         
         // Create a rectangle for the bucket hitbox
         mPlayer = new Rectangle();
-        mPlayer.x = 64; // center the bucket on x-axis
-        mPlayer.y = 67; // bottom left of the bucket is 20px above floor
+        
+        mPlayerX = 0;
+        mPlayerY = 0;
+        
+        //mPlayerPosition = new char[][] {{0}};
+        //mPlayerPosition[mPlayerX][mPlayerY] = 1;
         
         mPlayer.width = 32;
         mPlayer.height = 32;
     }
     
-    private void initTextures() {
-        mWaterTile = new Tile(new Texture(Gdx.files.internal("water2.png")));
+    private void initSounds() {
+        mInvalidSound = Gdx.audio.newSound(Gdx.files.internal("invalid.mp3"));
+        mSelectionSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+        mFirstLevelMusic = Gdx.audio.newMusic(Gdx.files.internal(
+         "first_level.mp3"));
+        mFirstLevelMusic.setLooping(true); 
+    }
+    
+    private void initTextures() {        
+        mMtnTexture = new Texture(Gdx.files.internal("mountain.png"));
         mGrassTile = new Tile(new Texture(Gdx.files.internal("grass.png")));
         mDirtTile = new Tile(new Texture(Gdx.files.internal("dirt.png")));
         mDirtTile2 = new Tile(new Texture(Gdx.files.internal("dirt2.png")));
         mDirtTile3 = new Tile(new Texture(Gdx.files.internal("dirt3.png")));
         mBridgeTile = new Tile(new Texture(Gdx.files.internal("bridge.png")));
         mOrangeTreeTile = new Tile(new Texture(Gdx.files.internal("tree.png")));        
-        mPlayerImage = new Texture(Gdx.files.internal("player.png"));
+        mWaterTile = new Tile(new Texture(Gdx.files.internal("water.png")));
+        
+        mWaterTile.setTerrainType(1);
+        mDirtTile.setTerrainType(2);
+        mBridgeTile.setTerrainType(3);
+        
+        mPlayerImage = new Texture(Gdx.files.internal("player.png"));        
     }
     
     private void initSpriteAnimation() {
         mPlayerSheet = new Texture(Gdx.files.internal("player.png"));
         mWaterSheet = new Texture(Gdx.files.internal("water2.png"));
+        mMountainSheet = new Texture(Gdx.files.internal("mountain.png"));
         
         TextureRegion[][] tmp = TextureRegion.split(mPlayerSheet, 
          mPlayerSheet.getWidth()/FRAME_COLS, 
          mPlayerSheet.getHeight()/FRAME_ROWS);  
         
         TextureRegion[][] wtr = TextureRegion.split(mWaterSheet, 
-         mWaterSheet.getWidth() / WATER_COLS, mWaterSheet.getHeight() / WATER_ROWS);
+         mWaterSheet.getWidth() / WATER_COLS, 
+         mWaterSheet.getHeight() / WATER_ROWS);
         
+
         mPlayerFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
-        mWaterFrames = new TextureRegion[WATER_COLS * WATER_ROWS];
+        mWaterFrames = new TextureRegion[WATER_COLS * WATER_ROWS];        
         
         int index = 0;
         for (int i = 0; i < FRAME_ROWS; i++) {
@@ -167,8 +235,8 @@ public class GameScreen implements Screen {
             // This method lays out the tiles
             // based on the map array
             generateMap();
-            game.batch.draw(mMenuBar, 20, 20);
-            game.batch.draw(mCurrentPlayerFrame, mPlayer.x, mPlayer.y);
+            //game.batch.draw(mMenuBar, 20, 20);
+            game.batch.draw(mCurrentPlayerFrame, mPlayerX, mPlayerY);
             game.batch.draw(mPlayerAnimation.getKeyFrame(delta, true), 0, 0);
 
             game.batch.end();
@@ -178,8 +246,23 @@ public class GameScreen implements Screen {
     }
     
     private void generateMap() {
-        for (int i = mWorld.length - 1; i >= 0; i--) {
-            for (int j = mWorld[i].length - 1; j >= 0; j--) {
+        for (int i = 0; i < mWorld.length; i++) {
+            for (int j = 0; j < mWorld[i].length; j++) {
+                if (mWorld[i][j].equals(mDirtTile)) {
+                    game.batch.draw(mDirtTile.getImage(), i * TILE_SIZE, j * TILE_SIZE);
+                }
+                else if (mWorld[i][j].equals(mWaterTile)) {
+                    game.batch.draw(mWaterAnimation.getKeyFrame(mStateTime, true), i * TILE_SIZE, j * TILE_SIZE);
+                }
+                else if (mWorld[i][j].equals(mGrassTile)) {
+                    game.batch.draw(mGrassTile.getImage(), i * TILE_SIZE, j * TILE_SIZE);
+                }
+                else if (mWorld[i][j].equals(mBridgeTile)) {
+                    game.batch.draw(mBridgeTile.getImage(), i * TILE_SIZE, j * TILE_SIZE);
+                }
+                
+                /*
+                
                 switch (mWorld[i][j]) {
                     case 0:
                         game.batch.draw(mGrassTile.getImage(), j * TILE_SIZE,
@@ -207,43 +290,93 @@ public class GameScreen implements Screen {
                     case 6:
                         game.batch.draw(mDirtTile3.getImage(), j * TILE_SIZE, i * TILE_SIZE);
                         break;
+                    case 7:
+                        game.batch.draw(mMountainSheet, j * TILE_SIZE, i * TILE_SIZE);
+                        break;
                     default:
                         break;
-                }
+                        
+                }*/
             }
         }
     }
     
-    public void checkPlayerMovement() {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {
-            mSelectionSound.play();
-            mPlayer.x -= TILE_SIZE;
+    public void checkPlayerMovement() {  
+        if (Gdx.input.isKeyJustPressed(Input.Keys.C)) {                       
+            System.out.println("mPlayerX:" + mPlayerX + ", mPlayerY: " + mPlayerY); 
+            System.out.println("Array: " + mWorld[mPlayerX/32][mPlayerY/32]);
+        }
+        
+        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {                       
+            if((mPlayerX / TILE_SIZE) - 1 < 0) {
+                mInvalidSound.play();
+            }            
+            else if(mWorld[(mPlayerX / TILE_SIZE) - 1][(mPlayerY / TILE_SIZE)].equals(mWaterTile)) {
+                mInvalidSound.play();
+            }
+            else {
+               mSelectionSound.play(); 
+               mPlayerX -= TILE_SIZE;      
+            }                               
         }
 
-        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
-            mSelectionSound.play();
-            mPlayer.x += TILE_SIZE;
+        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {                                                          
+            // First check if player is at the edge of the map
+            if((mPlayerX / TILE_SIZE) + 1 >= SCREEN_WIDTH_PX / TILE_SIZE) {
+                mInvalidSound.play();
+            }            
+            else if(mWorld[(mPlayerX / TILE_SIZE) + 1][(mPlayerY / TILE_SIZE)].equals(mWaterTile)) {
+                mInvalidSound.play();
+            }
+            else {
+                mSelectionSound.play(); 
+                mPlayerX += TILE_SIZE;
+            }            
         }
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
-            mSelectionSound.play();
-            mPlayer.y += TILE_SIZE;
+            if((mPlayerY / TILE_SIZE) + 1 >= SCREEN_HEIGHT_PX / TILE_SIZE) {
+                mInvalidSound.play();
+            }            
+            else if(mWorld[(mPlayerX / TILE_SIZE)][(mPlayerY / TILE_SIZE) + 1].equals(mWaterTile)) {
+                mInvalidSound.play();
+            }
+            else {
+                mSelectionSound.play();
+                mPlayerY += TILE_SIZE;
+            }     
         }
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
-            mSelectionSound.play();
-            mPlayer.y -= TILE_SIZE;
-        }
+            if((mPlayerY / TILE_SIZE) - 1 < 0) {
+                mInvalidSound.play();
+            }            
+            else if(mWorld[mPlayerX / TILE_SIZE][(mPlayerY / TILE_SIZE) - 1].equals(mWaterTile)) {
+                mInvalidSound.play();
+            }
+            else {
+               mSelectionSound.play();
+               mPlayerY -= TILE_SIZE;
+            }                         
+        }                
     }
-    
+
     public void checkPlayerBounds() {
-        if (mPlayer.x < 0) {
-            mPlayer.x = 0;
+        if (mPlayerX < 0) {
+            mPlayerX = 0;
+        }
+        
+        if (mPlayerY < 0) {
+            mPlayerY = 0;
+        }
+        
+        if (mPlayerY > SCREEN_HEIGHT_PX - TILE_SIZE) {
+            mPlayerY = SCREEN_HEIGHT_PX - TILE_SIZE;
         }
 
-        if (mPlayer.x > 800 - 32) {
-            mPlayer.x = 800 - 32;
-        }    
+        if (mPlayerX > SCREEN_WIDTH_PX - TILE_SIZE) {
+            mPlayerX = SCREEN_WIDTH_PX - TILE_SIZE;
+        }                    
     }
     
     public void show() {
--- Tile.java
+++ Tile.java
@@ -7,10 +7,31 @@ public class Tile {
     private int mTerrainType;
     private int mMovementCost;
     private Texture mTexture;
+    private boolean mIsPassable;
+    
+    public String toString() {
+        if (mTerrainType == 1) {
+            return "WATER TILE";
+        }
+        else if (mTerrainType == 2) {
+            return "DIRT TILE";
+        }
+        else if (mTerrainType == 3) {
+            return "BRIDGE";
+        }
+        else {
+            return "MISC TILE";
+        }
+    }
+    
+    public void setTerrainType(int type) {
+        mTerrainType = type;
+    }
     
     public Tile() {
         mTerrainType = 0;
         mMovementCost = 1;
+        mIsPassable = true; // All tiles passable by default
         mTexture = new Texture(Gdx.files.internal("grass.png"));
     }
     
@@ -22,5 +43,9 @@ public class Tile {
     
     public Texture getImage() {
         return mTexture;
+    }
+    
+    public void setPassable(boolean flag) {
+        mIsPassable = flag;
     }
 }
